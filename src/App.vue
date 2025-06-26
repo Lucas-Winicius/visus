32d4c44fc27ad013480cde10082fc51b35678e3d
Lucas Winicius <lucasclucas7@gmail.com>
2025-06-25 21:21:47 -0300

    :wrench: chore: tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,19 @@ import axios from "axios";
 import { onMounted } from "vue";
 
 onMounted(() => {
-  axios(process.env.API_URL)
+  axios(import.meta.env.VITE_API_URL + '/imagesByTime').then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
 })
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   <div class="bg-blue-500 text-white p-10 text-2xl">
+    Hello World!
   </div>
 </template>
 
@@ -25,9 +26,11 @@ onMounted(() => {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
