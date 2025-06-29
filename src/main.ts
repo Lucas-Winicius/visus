b7f5b91f885b157dfdcc28b601ff4f800efee9de
Lucas Winicius <lucasclucas7@gmail.com>
2025-06-29 14:35:00 -0300

    :wrench: chore: Vue router

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { OhVueIcon } from "oh-vue-icons";
 import "./style.css";
 import App from "./App.vue";
 import "./icons";
+import router from "./router";
 
 const app = createApp(App)
 
 app.component("v-icon", OhVueIcon)
+app.use(router)
 
 app.mount("#app")
