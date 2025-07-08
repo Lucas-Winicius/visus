42ecfd595bebc571e46fe50afebe3817a632968b
Lucas Winicius <lucasclucas7@gmail.com>
2025-07-08 20:13:43 -0300

    :sparkles: feat: Toaster

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import Toast from 'primevue/toast';
 import Header from './components/Header.vue';
 </script>
 
 <template>
   <Header />
   <router-view />
+  <Toast />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,23 @@ import "./style.css";
 import App from "./App.vue";
 import "./icons";
 import router from "./router";
+import { ToastService } from "primevue";
+import PrimeVue from "primevue/config";
+import Material from "@primeuix/themes/material";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.component("v-icon", OhVueIcon)
-app.use(router)
+app.component("v-icon", OhVueIcon);
+app.use(PrimeVue, {
+  theme: {
+    preset: Material,
+    options: {
+      darkModeSelector: ".dark-app",
+      cssLayer: false,
+    },
+  },
+});
+app.use(ToastService);
+app.use(router);
 
-app.mount("#app")
+app.mount("#app");
